0d9bffd93c947423985d5f1c2eacbe1d3abfe7df
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-08 16:09:39 +0200

    4.5 Serwis i repozytorium

--- src/main/java/pl/clockworkjava/gnomix/controllers/GuestController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/GuestController.java
@@ -1,20 +1,28 @@
 package pl.clockworkjava.gnomix.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.clockworkjava.gnomix.domain.guest.Gender;
 import pl.clockworkjava.gnomix.domain.guest.Guest;
+import pl.clockworkjava.gnomix.domain.guest.GuestService;
 
 import java.time.LocalDate;
 
 @Controller
 public class GuestController {
 
+    private GuestService guestService;
+
+    @Autowired
+    public GuestController(GuestService service) {
+        this.guestService = service;
+    }
+
     @GetMapping("/guests")
     public String guests(Model model) {
-        Guest guest = new Guest("Paweł", "Cwik", LocalDate.of(1986, 11, 13), Gender.MALE);
-        model.addAttribute("guest", guest);
+        model.addAttribute("guests", this.guestService.findAll());
         return "guests";
     }
 }
