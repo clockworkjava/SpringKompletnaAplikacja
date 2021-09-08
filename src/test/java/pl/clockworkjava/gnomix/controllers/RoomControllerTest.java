e509c1cd7386748915f8d28f797cee7a226141a4
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-08 22:16:50 +0200

    4.8 Cwiczenie praktyczne

--- src/test/java/pl/clockworkjava/gnomix/controllers/RoomControllerTest.java
+++ src/test/java/pl/clockworkjava/gnomix/controllers/RoomControllerTest.java
@@ -5,9 +5,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.clockworkjava.gnomix.domain.room.Room;
+import pl.clockworkjava.gnomix.domain.room.RoomService;
+
+import java.util.Arrays;
 
 @WebMvcTest(RoomController.class)
 public class RoomControllerTest {
@@ -15,11 +21,19 @@ public class RoomControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private RoomService roomService;
+
     @Test
     public void basic() throws Exception {
+
+        Room r = new Room("1408");
+
+        Mockito.when(roomService.findAll()).thenReturn(Arrays.asList(r));
+
         mockMvc.perform(get("/rooms"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("room"))
+                .andExpect(model().attributeExists("rooms"))
                 .andExpect(view().name("rooms"))
                 .andExpect(content().string(containsString("1408")));
     }
