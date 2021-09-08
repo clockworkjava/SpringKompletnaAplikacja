b78965946a3b2368e2672e3822762cbe42a43fcb
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-08 22:00:47 +0200

    4.7 Testy jednostkowe - mockowanie zaleznosci

--- src/test/java/pl/clockworkjava/gnomix/controllers/GuestControllerTest.java
+++ src/test/java/pl/clockworkjava/gnomix/controllers/GuestControllerTest.java
@@ -5,9 +5,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.clockworkjava.gnomix.domain.guest.Gender;
+import pl.clockworkjava.gnomix.domain.guest.Guest;
+import pl.clockworkjava.gnomix.domain.guest.GuestService;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @WebMvcTest(GuestController.class)
 public class GuestControllerTest {
@@ -15,11 +23,19 @@ public class GuestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private GuestService guestService;
+
     @Test
     public void basic() throws Exception {
+
+        Guest guest = new Guest("Paweł", "Cwik", LocalDate.of(1986, 11, 13), Gender.MALE);
+
+        Mockito.when(guestService.findAll()).thenReturn(Arrays.asList(guest));
+
         mockMvc.perform(get("/guests"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("guest"))
+                .andExpect(model().attributeExists("guests"))
                 .andExpect(view().name("guests"))
                 .andExpect(content().string(containsString("1986-11-13")));
     }
