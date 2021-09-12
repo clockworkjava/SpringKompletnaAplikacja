50325793d19b18ce1c1a312adfbb8b3a0ce427a5
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-12 13:56:57 +0200

    5.5 Post unit testing

--- src/test/java/pl/clockworkjava/gnomix/controllers/GuestControllerTest.java
+++ src/test/java/pl/clockworkjava/gnomix/controllers/GuestControllerTest.java
@@ -1,7 +1,9 @@
 package pl.clockworkjava.gnomix.controllers;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,11 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.clockworkjava.gnomix.controllers.dto.GuestCreationDTO;
 import pl.clockworkjava.gnomix.domain.guest.Gender;
 import pl.clockworkjava.gnomix.domain.guest.Guest;
 import pl.clockworkjava.gnomix.domain.guest.GuestService;
@@ -38,5 +44,29 @@ public class GuestControllerTest {
                 .andExpect(model().attributeExists("guests"))
                 .andExpect(view().name("guests"))
                 .andExpect(content().string(containsString("1986-11-13")));
+    }
+
+    @Test
+    public void handlePost() throws Exception {
+
+        String postContent = "firstName=Pawel&lastName=Cwik&dateOfBirth=2021-09-15&gender=FEMALE";
+
+        MockHttpServletRequestBuilder request =
+                post("/createNewGuest")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .content(postContent);
+
+        mockMvc.perform(request)
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("guests"));
+
+        GuestCreationDTO dto = new GuestCreationDTO(
+                "Pawel",
+                "Cwik",
+                LocalDate.parse("2021-09-15"),
+                Gender.FEMALE
+        );
+
+        Mockito.verify(guestService, Mockito.times(1)).createNewGuest(dto);
     }
 }
