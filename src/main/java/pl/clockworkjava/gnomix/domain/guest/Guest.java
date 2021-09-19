79a1e402851fc60577b7cc7804089f1f3bffa470
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-19 12:04:07 +0200

    6.1 Identyfikator

--- src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
@@ -7,9 +7,19 @@ import java.time.LocalDate;
 @Data
 public class Guest {
 
+    private final long id;
     private final String firstName;
     private final String lastName;
     private final LocalDate birthDate;
     private final Gender gender;
 
+    public Guest(String firstName, String lastName, LocalDate birthDate, Gender gender) {
+
+        this.id = System.currentTimeMillis();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+
+    }
 }
