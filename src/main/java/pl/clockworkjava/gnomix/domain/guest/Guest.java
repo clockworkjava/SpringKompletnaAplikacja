48b12801a2099354556948b10ee23bba1b4e4199
Pawel Cwik <pawel@clockworkjava.pl>
2021-09-30 17:57:00 +0200

    8.1 Configuracja Hibernate

--- src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/guest/Guest.java
@@ -4,18 +4,30 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
 @Setter(value = AccessLevel.NONE)
+@Entity
 public class Guest {
 
-    private final long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
     private Gender gender;
+
+    private Guest() {
+
+    }
 
     public Guest(String firstName, String lastName, LocalDate birthDate, Gender gender) {
 
