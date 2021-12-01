365b3636559611a189901bb3e74821980c059c39
Pawel Cwik <pawel@clockworkjava.pl>
2021-12-01 23:43:21 +0100

    9.4 Spring validation i nie tylko

--- src/main/java/pl/clockworkjava/gnomix/controllers/GuestController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/GuestController.java
@@ -3,12 +3,14 @@ package pl.clockworkjava.gnomix.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pl.clockworkjava.gnomix.controllers.dto.GuestCreationDTO;
 import pl.clockworkjava.gnomix.controllers.dto.GuestUpdateDTO;
 import pl.clockworkjava.gnomix.domain.guest.Guest;
 import pl.clockworkjava.gnomix.domain.guest.GuestService;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 
 @Controller
@@ -28,17 +30,22 @@ public class GuestController {
         return "guests";
     }
 
-        @GetMapping("/create")
+    @GetMapping("/create")
     public String createNewGuest() {
         return "createNewGuest";
     }
 
     @PostMapping
-    public String handleCreateNewGuest(GuestCreationDTO dto) {
+    public String handleCreateNewGuest(@Valid GuestCreationDTO dto, BindingResult result, Model model) {
 
-        this.guestService.createNewGuest(dto);
+        if(result.hasErrors()) {
+            model.addAttribute("errors", result.getAllErrors());
+            return "createNewGuest";
+        } else {
+            this.guestService.createNewGuest(dto);
+            return "redirect:/guests";
+        }
 
-        return "redirect:/guests";
     }
 
     @GetMapping("/delete/{id}")
--- src/main/java/pl/clockworkjava/gnomix/controllers/dto/GuestCreationDTO.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/dto/GuestCreationDTO.java
@@ -5,14 +5,20 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import pl.clockworkjava.gnomix.domain.guest.Gender;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class GuestCreationDTO {
 
+    @NotBlank
     private final String firstName;
+
+    @NotBlank
     private final String lastName;
 
+    @Past(message = "Data urodzenia musi być w przeszłości")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private final LocalDate dateOfBirth;
 
