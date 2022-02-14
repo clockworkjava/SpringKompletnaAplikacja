ec4a5aa0f0a6b0ea27a4fca49973ea597e782eda
clockworkjava <clockworkjava01@gmail.com>
2022-02-14 20:44:43 +0100

    12.1 Wysyłanie wiadomości email

--- src/main/java/pl/clockworkjava/gnomix/util/mail/HandleMailTempReservationCreateEvent.java
+++ src/main/java/pl/clockworkjava/gnomix/util/mail/HandleMailTempReservationCreateEvent.java
@@ -1,5 +1,6 @@
 package pl.clockworkjava.gnomix.util.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import pl.clockworkjava.gnomix.domain.reservation.events.TempReservationCreatedEvent;
@@ -7,8 +8,16 @@ import pl.clockworkjava.gnomix.domain.reservation.events.TempReservationCreatedE
 @Component
 public class HandleMailTempReservationCreateEvent implements ApplicationListener<TempReservationCreatedEvent> {
 
+    private final EmailService emailService;
+
+    @Autowired
+    public HandleMailTempReservationCreateEvent(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @Override
     public void onApplicationEvent(TempReservationCreatedEvent event) {
         System.out.println("MAIL: Handle event by implementing AppListener");
+        this.emailService.sendConfirmationEmail(event.getEmail(), event.getReservationId());
     }
 }
