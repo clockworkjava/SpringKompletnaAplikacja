32ac8c142ea819bcf176a2542740178431af27a9
clockworkjava <clockworkjava01@gmail.com>
2022-02-15 22:25:53 +0100

    12.2 Wstrzykiwanie danych o domenie

--- src/main/java/pl/clockworkjava/gnomix/util/mail/EmailService.java
+++ src/main/java/pl/clockworkjava/gnomix/util/mail/EmailService.java
@@ -1,6 +1,7 @@
 package pl.clockworkjava.gnomix.util.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,17 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender sender;
+
+    @Value("${gnomix.protocol}")
+    private String protocol;
+
+    @Value("${gnomix.domain}")
+    private String domain;
+
+    @Value("${gnomix.port}")
+    private String port;
+
+    private String confirmationEndpoint = "reservations/confirm";
 
     @Autowired
     public EmailService(JavaMailSender sender) {
@@ -19,12 +31,12 @@ public class EmailService {
 
         SimpleMailMessage mail = new SimpleMailMessage();
 
-        String text = "http://localhost:8080/reservations/confirm/"+reservationId;
+        String endpoint = String.format("%s://%s:%s/%s/%d", protocol, domain, port, confirmationEndpoint, reservationId);
 
         mail.setTo(email);
         mail.setFrom("systemgnomix@gmail.com");
         mail.setSubject("Potwierdź rezerwację");
-        mail.setText("Dziękujemy za dokonanie rezerwacji, by ją potwierdzić kliknij w link: " + text);
+        mail.setText("Dziękujemy za dokonanie rezerwacji, by ją potwierdzić kliknij w link: " + endpoint);
 
         this.sender.send(mail);
 
