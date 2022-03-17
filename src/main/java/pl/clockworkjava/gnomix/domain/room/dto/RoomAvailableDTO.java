4cd329e77dd4c8411433fdcfbee8c02443f11b3e
clockworkjava <clockworkjava01@gmail.com>
2022-03-17 11:18:04 +0100

    12.10 Rozbudowa rest dto

--- src/main/java/pl/clockworkjava/gnomix/domain/room/dto/RoomAvailableDTO.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/room/dto/RoomAvailableDTO.java
@@ -5,6 +5,7 @@ import pl.clockworkjava.gnomix.domain.room.BedType;
 import pl.clockworkjava.gnomix.domain.room.Room;
 
 import java.util.List;
+import java.util.Objects;
 
 public class RoomAvailableDTO {
 
@@ -12,12 +13,16 @@ public class RoomAvailableDTO {
     private final long id;
     private final List<BedType> beds;
     private final int size;
+    private final String description;
+    private final List<String> photosUrls;
 
-    public RoomAvailableDTO(String number, long id, List<BedType> beds, int size) {
+    public RoomAvailableDTO(String number, long id, List<BedType> beds, int size, String description, List<String> photosUrls) {
         this.number = number;
         this.id = id;
         this.beds = beds;
         this.size = size;
+        this.description = description;
+        this.photosUrls = photosUrls;
     }
 
     public RoomAvailableDTO(Room room) {
@@ -25,6 +30,8 @@ public class RoomAvailableDTO {
         this.id = room.getId();
         this.beds = room.getBeds();
         this.size = room.getSize();
+        this.description = room.getDescription();
+        this.photosUrls = room.getPhotosUrls();
     }
 
     @JsonProperty("roomNumber")
@@ -42,5 +49,26 @@ public class RoomAvailableDTO {
 
     public int getSize() {
         return size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getPhotosUrls() {
+        return photosUrls;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoomAvailableDTO that = (RoomAvailableDTO) o;
+        return id == that.id && size == that.size && Objects.equals(number, that.number) && Objects.equals(beds, that.beds) && Objects.equals(description, that.description) && Objects.equals(photosUrls, that.photosUrls);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, id, beds, size, description, photosUrls);
     }
 }
