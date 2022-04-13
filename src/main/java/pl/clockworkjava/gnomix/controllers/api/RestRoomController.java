808eda5a7a0be46713aec57d4c96c861db58d289
clockworkjava <clockworkjava01@gmail.com>
2022-04-14 00:00:36 +0200

    14.1 Pobieranie wszystkich pokoi

--- src/main/java/pl/clockworkjava/gnomix/controllers/api/RestRoomController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/api/RestRoomController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import pl.clockworkjava.gnomix.domain.room.RoomService;
 import pl.clockworkjava.gnomix.domain.room.dto.RoomAvailableDTO;
 import pl.clockworkjava.gnomix.domain.reservation.ReservationService;
 import pl.clockworkjava.gnomix.domain.room.Room;
@@ -18,10 +19,12 @@ import java.util.stream.Collectors;
 public class RestRoomController {
 
     private final ReservationService reservationService;
+    private final RoomService roomService;
 
     @Autowired
-    public RestRoomController(ReservationService service) {
+    public RestRoomController(ReservationService service, RoomService roomService) {
         this.reservationService = service;
+        this.roomService = roomService;
     }
 
     @GetMapping("api/getFreeRooms")
@@ -36,5 +39,10 @@ public class RestRoomController {
         } catch (IllegalArgumentException ex) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
         }
+    }
+
+    @GetMapping("api/rooms")
+    public List<RoomAvailableDTO> getAllRooms() {
+        return this.roomService.findAll().stream().map(RoomAvailableDTO::new).collect(Collectors.toList());
     }
 }
