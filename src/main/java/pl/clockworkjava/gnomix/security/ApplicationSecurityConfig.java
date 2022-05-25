50de26c11f9e8326bed3f0ab6df24c219501304b
clockworkjava <clockworkjava01@gmail.com>
2022-05-25 18:13:33 +0200

    17.6 PreAuthorize

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -32,10 +34,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/*")
                 .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/rooms/**")
-                .hasAnyRole("MANAGER", "RECEPTION")
-                .antMatchers("/api/rooms/**", "/rooms/**")
-                .hasRole("MANAGER")
                 .anyRequest()
                 .authenticated()
                 .and()
